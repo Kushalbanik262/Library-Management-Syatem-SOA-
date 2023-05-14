e0cd68974596c97ebf0870406a0fda57ad5e7543
kushal <kushalbanik93@gmail.com>
2023-05-14 14:28:08 +0530

    Microservices Bare Stucture Added

---
+++ FineMicroservice/src/main/java/com/example/FineMicroservice/FineMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.FineMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FineMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FineMicroserviceApplication.class, args);
+	}
+
+}
---
+++ FineMicroservice/src/test/java/com/example/FineMicroservice/FineMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FineMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FineMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
